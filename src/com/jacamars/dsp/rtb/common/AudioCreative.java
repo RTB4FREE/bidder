22150ddd9b3f29a01fc59a7f41e554fc171c7282
Ben Faul <ben.faul@gmail.com>
2019-05-24 18:35:40 -0700

    New creative

---
+++ src/com/jacamars/dsp/rtb/common/AudioCreative.java
@@ -0,0 +1,23 @@
+package com.jacamars.dsp.rtb.common;
+
+import java.util.List;
+
+public class AudioCreative {
+
+	public String mimeType;
+	public Integer duration;
+	public Integer protocol;
+	public Integer startdelay;
+	public Integer sequence;
+	public Integer maxextended;
+	public Integer bitrate;
+	public Integer delivery;
+	public Integer api;
+	public Integer maxseq;
+	public Integer feed;
+	public Integer stitched;
+	
+	public AudioCreative() {
+		
+	}
+}
